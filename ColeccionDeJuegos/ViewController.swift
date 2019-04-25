e7c838aeb82a6f80ea32d1bece19b07372f12166
Fernando Huarcaya Torres <nandohuarcaya@MacBook-Pro-de-Fernando.local>
2019-04-25 15:20:55 -0500

    iOS

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Fernando Huarcaya Torres on 4/25/19.
+//  Copyright © 2019 Fernando Huarcaya Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
